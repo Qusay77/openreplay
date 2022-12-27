20b7576510bcdbab6de6fddd5740e7a7ababf030
Qusay <qusay@nadsoft.net>
2022-12-27 12:10:00 +0300

    player block - sub header

--- frontend/app/components/Session_/PlayerBlock.js
+++ frontend/app/components/Session_/PlayerBlock.js
@@ -1,44 +1,27 @@
 import React from 'react';
-import cn from "classnames";
+import cn from 'classnames';
 import { connect } from 'react-redux';
-import {  } from 'Player';
-import {
-  NONE, OVERVIEW,
-} from 'Duck/components/player';
+import {} from 'Player';
+import { NONE } from 'Duck/components/player';
 import Player from './Player';
-import SubHeader from './Subheader';
-
 import styles from './playerBlock.module.css';
 
-@connect(state => ({
-  fullscreen: state.getIn([ 'components', 'player', 'fullscreen' ]),
-  bottomBlock: state.getIn([ 'components', 'player', 'bottomBlock' ]),
-  sessionId: state.getIn([ 'sessions', 'current', 'sessionId' ]),
-  disabled: state.getIn([ 'components', 'targetDefiner', 'inspectorMode' ]),
-  jiraConfig: state.getIn([ 'issues', 'list' ]).first(),
+@connect((state) => ({
+  fullscreen: state.getIn(['components', 'player', 'fullscreen']),
+  bottomBlock: state.getIn(['components', 'player', 'bottomBlock']),
+  sessionId: state.getIn(['sessions', 'current', 'sessionId']),
+  disabled: state.getIn(['components', 'targetDefiner', 'inspectorMode']),
+  jiraConfig: state.getIn(['issues', 'list']).first(),
 }))
 export default class PlayerBlock extends React.PureComponent {
   render() {
-    const {
-      fullscreen,
-      bottomBlock,
-      sessionId,
-      disabled,
-      activeTab,
-      jiraConfig,
-     } = this.props;
+    const { fullscreen, bottomBlock, activeTab } = this.props;
 
     return (
-      <div className={ cn(styles.playerBlock, "flex flex-col overflow-x-hidden") }>
-          {!fullscreen && <SubHeader
-            sessionId={sessionId}
-            disabled={disabled}
-            jiraConfig={jiraConfig}
-          />}
+      <div className={cn(styles.playerBlock, 'flex flex-col overflow-x-hidden')}>
         <Player
           className="flex-1"
-          bottomBlockIsActive={ !fullscreen && bottomBlock !== NONE }
-          // bottomBlockIsActive={ true }
+          bottomBlockIsActive={!fullscreen && bottomBlock !== NONE}
           bottomBlock={bottomBlock}
           fullscreen={fullscreen}
           activeTab={activeTab}
